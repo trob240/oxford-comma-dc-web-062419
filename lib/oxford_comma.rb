c1a5e9105ea7f5780f7e055f9c0edc8e9b17ee3b
Trevahr Robertson <trevahr@yahoo.com>
2019-04-28 23:14:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,14 @@ def oxford_comma(array)
 if array.count == 1
   array.join
 
-elsif array.count > 2
+elsif array.count == 2
   array.join(" and ")
 
-elsif array.count == 3
+elsif array.count > 2
 
-  
+ array.count 
+
+  array.join(", ")
 
 
 
