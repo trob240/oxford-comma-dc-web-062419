2a4c5a473e2e88b1ac9355482cb340be7f6ec86e
Trevahr Robertson <trevahr@yahoo.com>
2019-04-28 23:35:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
 
+if array.count == 1
+  array.join
+
+elsif array.count == 2
+  array.join(" and ")
+
+elsif array.count > 2
+
+i = 0
+b = []
+while i < (array.count - 1)
+
+b.push(array[i])
+i += 1
+end
+
+  b.push("and #{array[(array.count - 1)]}")
+
+  b.join(", ")  
+end
+
 end
