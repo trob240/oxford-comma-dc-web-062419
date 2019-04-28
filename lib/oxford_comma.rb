b494350762d39fabc74f20308b43c5f94bd10600
Trevahr Robertson <trevahr@yahoo.com>
2019-04-28 23:28:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,11 +10,17 @@ elsif array.count > 2
 
 i = 0
 b = []
-while i <= (array.count - 1)
+while i < (array.count - 2)
 
 b.push(array[i])
+i += 1
+end
 
-  array.join(", ")
+  b.push("and #{array[(array.count - 1)]}")
+
+  b.join(", ")
+  
+  b
 
 
 
